5edd17ae4d01a3e5b4828a9abce9001fa3fe9350
arielvasconcelosgoncalves <ariel.goncalves@sou.unifeob.edu.br>
2025-10-09 19:06:00 -0300

    fix: trocando enum por const enum

--- interface/src/types/transactions.ts
+++ interface/src/types/transactions.ts
@@ -1,48 +1,47 @@
 import type { Category, CategorySummary } from "./category";
 
-export enum TransactionType{
-    EXPENSE = "expense",
-    INCOME = "income",
+export const enum TransactionType {
+  EXPENSE = "expense",
+  INCOME = "income",
 }
 
-export interface Transaction{
-    id: string;
-    description: string;
-    amount: number;
-    date: string | Date;
-    categoryId: string;
-    type: TransactionType;
-    createdAt: string | Date;
-    updateAt: string | Date;
-    category: Category;
-    userId: string
-    
+export interface Transaction {
+  id: string;
+  description: string;
+  amount: number;
+  date: string | Date;
+  categoryId: string;
+  type: TransactionType;
+  createdAt: string | Date;
+  updateAt: string | Date;
+  category: Category;
+  userId: string;
 }
 
-export interface createTransactionDTO{
-    description: string,
-    amount: number,
-    date: Date | string,
-    categoryId: string,
-    type: TransactionType;
+export interface createTransactionDTO {
+  description: string;
+  amount: number;
+  date: Date | string;
+  categoryId: string;
+  type: TransactionType;
 }
 
-export interface TransactionFilter{
-    month: number;
-    year: number;
-    categoryId?: string;
-    type?: TransactionType;
+export interface TransactionFilter {
+  month: number;
+  year: number;
+  categoryId?: string;
+  type?: TransactionType;
 }
 
-export interface TransactionSummary{
-    totalExpenses: number;
-    totalIncomes: number,
-    balance: number,
-    expensesByCategory: CategorySummary[];
+export interface TransactionSummary {
+  totalExpenses: number;
+  totalIncomes: number;
+  balance: number;
+  expensesByCategory: CategorySummary[];
 }
 
 export interface MonthlyItem {
-    name: string;
-    expenses: number;
-    income: number;
+  name: string;
+  expenses: number;
+  income: number;
 }
